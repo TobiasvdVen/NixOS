02b1f680f7950d68ca4787d52d10aa02b4ee443d
TobiasvdVen <tobiasvdven@hotmail.com>
2026-07-24 16:55:05 +0200

    Upgrade to Nix 26.05

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Root NixOS flake, which can compose the following distinct configurations: 'personal', ...";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     passivate-git = {
       url = "git+https://github.com/TobiasvdVenOrg/Passivate?ref=main&submodules=1";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
 
       inputs.nixpkgs.follows = "nixpkgs";
     };
