cac6a42d179bfe1892e888dae0f25fcfd0261498
TobiasvdVen <tobiasvdven@hotmail.com>
2026-08-24 17:15:09 +0200

    Missed that I was loading the wrong flake in rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,4 @@ set -e
 
 rm -f ./modules/system/hardware-configuration.nix
 cp /etc/nixos/hardware-configuration.nix ./modules/system
-nixos-rebuild --flake path:./tobias-pc#tobias-pc dry-activate
+nixos-rebuild --flake path:.#tobias-pc dry-activate
