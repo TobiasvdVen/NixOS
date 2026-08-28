dcd106198134344ada0ba5bc0a5ff44dc3913f84
TobiasvdVen <tobiasvdven@hotmail.com>
2026-08-28 21:07:07 +0200

    Convert root flake to flake-parts

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-tools-path.url = "path:./nixos_tools";
     nil-git.url = "github:oxalica/nil";
     tobias-pc.url = "path:./tobias-pc";
@@ -10,23 +11,44 @@
 
   outputs =
     inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import inputs.nixpkgs { inherit system; };
-      nt = inputs.nixos-tools-path.packages.${system}.nt;
-      nixos_tools = inputs.nixos-tools-path.packages.${system}.nixos_tools;
-      nil = inputs.nil-git.packages.${system}.default;
-    in
-    {
-      nixosConfigurations.tobias-pc = inputs.tobias-pc;
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        config,
+        withSystem,
+        moduleWithSystem,
+        ...
+      }:
+      {
+        imports = [
 
-      devShells."${system}".default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.package-version-server
-          nt
-          nixos_tools
-          nil
         ];
-      };
-    };
+
+        flake = {
+          nixosConfigurations.tobias-pc = inputs.tobias-pc;
+        };
+
+        systems = [
+          "x86_64-linux"
+        ];
+
+        perSystem =
+          {
+            config,
+            pkgs,
+            inputs',
+            ...
+          }:
+          {
+
+            devShells.default = pkgs.mkShell {
+              buildInputs = [
+                pkgs.package-version-server
+                inputs'.nixos-tools-path.nt
+                inputs'.nixos-tools-path.nixos_tools
+                inputs'.nixos-tools-path.packages.nil
+              ];
+            };
+          };
+      }
+    );
 }
