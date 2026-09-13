1568dcce547309629f33374fc346440b6cce78e9
TobiasvdVen <tobiasvdven@hotmail.com>
2026-09-13 21:15:17 +0200

    Revert "Wtf"
    This reverts commit 379422e8d29ba0c8c37e095dabaed5fcf357d35e.

--- .nvim.lua
+++ .nvim.lua
@@ -1,1 +1,42 @@
-error("AAAAAAAAA")
+vim.print("LOADED .nvim.lua")
+
+local project_actions = require("project_actions")
+
+local some_task = {
+        name = "Some Task",
+        builder = function(params)
+                return {
+                        cmd = { "echo", "hello", "world" },
+                        name = "Greet",
+                        components = { "default" },
+                }
+        end,
+        desc = "Optional description of task",
+        condition = {
+                filetype = { "nix" },
+        },
+}
+
+local some_task_2 = {
+        name = "Some Task 2",
+        builder = function(params)
+                return {
+                        cmd = { "echo", "hello", "world" },
+                        name = "Greet2",
+                        components = { "default" },
+                }
+        end,
+        desc = "Optional description of task",
+        condition = {
+                filetype = { "nix" },
+        },
+}
+
+local action_leader = "<leader>b"
+
+local actions = {
+        some_task,
+        some_task_2,
+}
+
+project_actions.register(action_leader, actions)
